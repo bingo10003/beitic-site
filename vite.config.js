acb16ecfc3a433c24e3f60d5a608182ff9f25758
bingo10003 <to520323520@gmail.com>
2025-04-06 19:23:44 +0800

    Fix Vite base path for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: './', // <-- 這一行是關鍵！
   plugins: [react()],
 })
